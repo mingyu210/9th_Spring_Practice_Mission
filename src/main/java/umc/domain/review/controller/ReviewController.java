77385d18c05a792f26943da435b3a68b63ed079f
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2025-11-09 11:00:23 +0900

    refactor: ReviewController가 엔티티 대신 DTO를 반환하도록 수정

--- src/main/java/umc/domain/review/controller/ReviewController.java
+++ src/main/java/umc/domain/review/controller/ReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import umc.domain.review.dto.ReviewResponseDTO;
 import umc.domain.review.entity.Review;
 import umc.domain.review.service.ReviewQueryService;
 
@@ -19,12 +20,12 @@ public class ReviewController {
     private final ReviewQueryService reviewQueryService;
 
     @GetMapping("/reviews/search")
-    public ResponseEntity<List<Review>> searchReview(
+    public ResponseEntity<List<ReviewResponseDTO>> searchReview(
             @RequestParam Long memberId,              // ✅ 추가됨
             @RequestParam(required = false) String type,
             @RequestParam(required = false) String query
     ){
-        List<Review> result = reviewQueryService.searchReview(memberId, type, query);
+        List<ReviewResponseDTO> result = reviewQueryService.searchReview(memberId, type, query);
         return ResponseEntity.ok(result);
     }
 }
